498a422d16f2b6b929e960131998728675b1fbeb
Mohammad Darvishi <mdarvishi5124@gmail.com>
2025-11-15 20:04:51 +0330

    Add files via upload

---
+++ Arad/html/script.js
@@ -0,0 +1,22 @@
+	<script>
+		function toggleDropdown() {
+			var dropdown = document.getElementById("dropdown");
+			if (dropdown.style.display === "block") {
+				dropdown.style.display = "none";
+			} else {
+				dropdown.style.display = "block";
+			}
+		}
+	
+		window.onclick = function(event) {
+			if (!event.target.closest('.dropdown-btn')) {
+				var dropdowns = document.getElementsByClassName("dropdown-content");
+				for (var i = 0; i < dropdowns.length; i++) {
+					var openDropdown = dropdowns[i];
+					if (openDropdown.style.display === "block") {
+						openDropdown.style.display = "none";
+					}
+				}
+			}
+		}
+	</script>
